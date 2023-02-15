f27f9e3e47030e94721d4a5959db8910e5dbc6e9
westshae <shaewest02@gmail.com>
2023-02-15 14:53:41 +1300

    Portfolio git formatting complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { DatabaseConnectionService } from "./database-connection.service";
 import { AuthModule } from "./auth/auth.module";
-import { VideosModule } from "./videos/videos.module";
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { BlogModule } from './blog/blog.module';
 
@@ -14,11 +11,8 @@ import { BlogModule } from './blog/blog.module';
       useClass: DatabaseConnectionService,
     }),
     AuthModule,
-    VideosModule,
     PortfolioModule,
     BlogModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -2,18 +2,62 @@ import { Injectable } from "@nestjs/common";
 import "dotenv/config";
 import axios from "axios";
 
+interface formattedRepo{
+  id:string,
+  name:string,
+  fullname:string,
+  description:string,
+  url:string,
+  language:string,
+  archived:boolean,
+  updatedAt:string,
+  languages:Array<{name:string, lines:number}>,
+}
+
 @Injectable()
 export class PortfolioService {
   async queryGithub() : Promise<any>{    
     try{
       let response = await axios
-      .get("https://api.github.com/users/westshae/repos");
-      return response.data;
+      .get("https://api.github.com/users/westshae/repos",
+      {
+        headers:{
+          Authorization: `Bearer ${process.env.GITPERSONALACCESSTOKEN}`,
+        }
+      });
+      return this.formatGithub(response.data);
     }catch(e){
       console.error(e);
     }
   }
-  formatGithub(data){
-    return data;
+  async formatGithub(data){
+    let collection:Array<formattedRepo> = [];
+
+    for(let repo of data){
+      let reponse = await axios
+        .get("https://api.github.com/repos/westshae/" + repo.name + "/languages",
+        {
+          headers:{
+            Authorization: `Bearer ${process.env.GITPERSONALACCESSTOKEN}`,
+          }
+        });
+
+
+      collection.push({
+        id: repo.id,
+        name: repo.name,
+        fullname: repo.full_name,
+        description: repo.description,
+        url: repo.html_url,
+        language: repo.language,
+        archived: repo.archived,
+        updatedAt: repo.pushed_at,
+        languages: reponse.data        
+      });
+    }
+
+    collection.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+
+    return collection;
   }
 }
